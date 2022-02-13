9d3433db8fcf18e69baef6dad9ddab25883ced44
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2022-02-13 01:05:27 +0100

    something
    ds

---
+++ compleanno.js
@@ -0,0 +1,14 @@
+
+var cerca = document.getElementById("cerca");
+var non_cliccare = document.getElementById("non_cliccare");
+
+cerca.addEventListener("click", cercaFunction);
+non_cliccare.addEventListener("click", nonCliccareFunction);
+
+function cercaFunction(){
+    alert("ancora non l'ho imparato questo")
+}
+
+function nonCliccareFunction(){
+    alert("scemo")
+}
